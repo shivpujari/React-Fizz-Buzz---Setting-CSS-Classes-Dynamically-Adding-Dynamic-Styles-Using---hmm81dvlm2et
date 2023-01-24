f0facd5458ea149e1c8d6c06ee2f929544524955
shiv pujari <shivanipujari03@gmail.com>
2023-01-24 16:00:21 +0530

    donebyme

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/App.css';
 const App = () => {
-
+  const [inc, setInc] = useState(0)
   
+  function Increment() {
+
+    setInc(inc + 1)
+  }
+  function Decrement() {
+    setInc(inc - 1)
+  }
   return (
     <div id="main">
-
+      <button id='increment' onClick={Increment} >Increment</button>
+      {
+        (inc % 15 === 0 && (<div id="counter" className='fizzbuzz' >{inc}</div>)) ||
+    (inc % 5 === 0 && (<div id="counter" className='buzz'>{inc}</div>)) ||
+    (inc % 3 === 0 && (<div id="counter" className='fizz'>{inc}</div>)) ||
+    (inc % 3 !==  0 && inc % 5 !== 0 && inc % 15 !== 0 &&(<div id="counter" className='normal'>{inc}</div>))
+   }
+      <button id='decrement' onClick={Decrement}>Decrement</button>
     </div>
   )
 }
+
+
 
 
 export default App;
